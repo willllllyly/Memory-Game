5e0a83305b9712a454298862c3e5e8298ba04005
willllllyly <el22wb@leeds.ac.uk>
2024-04-18 15:30:50 +0100

    Added Objects

--- main.cpp
+++ main.cpp
@@ -10,5 +10,19 @@ void ModeSelect();
 void ModeEasy();
 void ModeMedium();
 void ModeHard();
+void Button_isr();
+volatile int ButtonFlag = 0; //Flag for turning it on and off
 
 // Objects
+N5110 lcd(PC_7, PA_9, PB_10, PB_5, PB_3, PA_10);
+DigitalIn JoystickSwitch(PC_12); // Input for joystick switch
+Joystick Joystick(PC_1, PC_0);  // Joystick control
+InterruptIn Button(PC_10);
+DigitalOut GreenLED(PC_3);
+DigitalOut RedLED1(PA_13);
+DigitalOut RedLED2(PA_14);
+DigitalOut RedLED3(PA_15);
+PwmOut buzzer(PC_8);
+
+
+
