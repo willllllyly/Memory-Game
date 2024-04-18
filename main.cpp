76b6c0f9004b04cb265fed340f90d4166ed42e08
willllllyly <el22wb@leeds.ac.uk>
2024-04-18 16:08:12 +0100

    Pre On/Off Button

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ void ModeMedium();
 void ModeHard();
 void Button_isr();
 volatile int ButtonFlag = 0; //Flag for turning it on and off
+int State = 0;
 
 // Objects
 N5110 lcd(PC_7, PA_9, PB_10, PB_5, PB_3, PA_10);
@@ -24,5 +25,25 @@ DigitalOut RedLED2(PA_14);
 DigitalOut RedLED3(PA_15);
 PwmOut buzzer(PC_8);
 
+int main() {
+    
+    init(); // Initialise the device and certain objects
+    OpenGraphic();
+    //HowToPlay();
+    //ModeSelect();
+
+}
+
+void init() {
+    Joystick.init();
+    lcd.init(LPH7366_1);
+    lcd.setContrast(0.5);
+    lcd.setBrightness(0.5);
+    lcd.clear();
+}
+
+void OpenGraphic() {
+
+}
 
 
